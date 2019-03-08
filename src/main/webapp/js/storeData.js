81ad53da2aa01fd01bb0dd451453e3e7eed798c3
Mor1aty <971896352@qq.com>
2019-03-08 22:24:58 +0800

    baseURL modified

--- src/main/webapp/js/storeData.js
+++ src/main/webapp/js/storeData.js
@@ -1,6 +1,6 @@
-//var baseURL = "http://39.105.114.238:8080/brushing/";
+var baseURL = "http://39.105.114.238:8080/brushing/";
 //var baseURL = "http://210.47.163.67:8080/brushing/";
-var baseURL = "http://localhost:8080/brushing/";
+//var baseURL = "http://localhost:8080/brushing/";
 //var baseURL = "http://210.47.163.67/tomcat/brushing/";
 var storeData = {
 }
